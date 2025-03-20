939757d174cc12bb2f8e40c2a5a88d9585a2f165
Matheus Guilherme <53393804+matheusmbp-sre@users.noreply.github.com>
2025-03-20 00:46:57 -0300

    Update main.tf

--- fastapi/main.tf
+++ fastapi/main.tf
@@ -21,7 +21,7 @@ resource "datadog_monitor" "error_percentage" {
   type    = "metric alert"
   message = "An increased number of errors has been detected in the service named ${var.service_name}.\n Please, check the logs and latest deployments \n\n ${var.notification_channels}" 
 
-  query = "avg(last_30m):(sum:trace.aiohttp.request.errors{service:${var.service_name}, env:${var.environment}}.as_rate() * 100) / sum:trace.aiohttp.request.hits{service:${var.service_name}, env:${var.environment}}.as_rate() > ${var.error_critical_threshold}"
+  query = "avg(last_30m):(sum:trace.fastapi.request.errors{service:${var.service_name}, env:${var.environment}}.as_rate() * 100) / sum:trace.fastapi.request.hits{service:${var.service_name}, env:${var.environment}}.as_rate() > ${var.error_critical_threshold}"
 
   monitor_thresholds {
     critical = var.error_critical_threshold
@@ -39,7 +39,7 @@ resource "datadog_monitor" "latency" {
   type    = "query alert"
   message = "${var.service_name} has an abnormal change in latency for $env. The 75th percentile latency has deviated significantly. \n\n ${var.notification_channels}" 
 
-  query = "avg(last_12h):anomalies(p75:trace.aiohttp.request{service:${var.service_name}, env:${var.environment}}.as_count(), 'agile', 2, direction='both', interval=120, alert_window='last_30m', count_default_zero='true', seasonality='hourly') > 0.75"
+  query = "avg(last_12h):anomalies(p75:trace.fastapi.request{service:${var.service_name}, env:${var.environment}}.as_count(), 'agile', 2, direction='both', interval=120, alert_window='last_30m', count_default_zero='true', seasonality='hourly') > 0.75"
 
   monitor_thresholds {
     critical          = 0.75
@@ -79,7 +79,7 @@ resource "datadog_monitor" "request" {
   type    = "query alert"
   message = "An increased number of requests has been detected in the service named ${var.service_name}.\n Please contact Sec team, we might be under attack \n\n ${var.notification_channels}" 
 
-  query = "avg(last_30m):sum:trace.aiohttp.request.hits{service:${var.service_name}, env:${var.environment}}.as_rate() > ${var.request_critical_threshold}"
+  query = "avg(last_30m):sum:trace.fastapi.request.hits{service:${var.service_name}, env:${var.environment}}.as_rate() > ${var.request_critical_threshold}"
 
   monitor_thresholds {
     critical = var.request_critical_threshold
